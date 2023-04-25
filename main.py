51390c70ec5df869c793f1d21c278dfd6e9b948b
AnasBrrm <118625155+AnasBrrm@users.noreply.github.com>
2023-04-25 19:46:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,257 +1,260 @@
-import random
-def valeur():
-    global list_bloc, grid, bloc, Testjeu, score, reponse 
-    list_bloc = [[[[1,4,4],[1,4,4],[1,1,1],]],#
-                [[1,4,4],[1,1,4]],  #Petit L
-                [[1,1,1],[4,1,4]],  #T
-                [[1,1,1]],  #_
-                [[1,1,1],[1,1,1],[1,1,1]],  #Carré
-                [[1,1,4],[1,1,4]],  #Petit carré
-                [[1,1,4],[4,1,1]]] #Z
-    grid = []
-    bloc = []
-    Testjeu = False
-    reponse = 0
-    score = 0
-#Fonctions#aa
-def read_grid(path, grid):
+import pygame
+import sys
 
-    if path == 1:
-        with open(r"losange.txt", "r") as f1:
-            contenus = f1.readlines()
-    elif path == 2:
-        with open(r"cercle.txt", "r") as f1:
-            contenus = f1.readlines()
-    elif path == 3 :
-        with open(r"triangle.txt", "r") as f1:
-            contenus = f1.readlines()
-    for ligne in contenus:
-        L = ligne.split(" ")
-        L[-1] = L[-1][:-1] #supprimer l'anti slash n
-        grid.append(L)
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            try:
-                grid[i][j]=int(grid[i][j])
-            except :
-                pass
-        
-def save_grid(path, grid):
-    if path == 1:
-        with open(r"losange.txt", "w") as f2:
-            for i in range(len(grid)):
-                for j in range(len(grid[i])):
-                    f2.write(grid[i][j])
-    elif path == 2:
-        with open(r"cercle.txt", "w") as f2:
-            for i in range(len(grid)):
-                for j in range(len(grid[i])):
-                    f2.write(grid[i][j])
-    elif path == 3:
-        with open(r"triangle.txt", "w") as f2:
-            for i in range(len(grid)):
-                for j in range(len(grid[i])):
-                    f2.write(grid[i][j])
+# Initialisation de Pygame
+pygame.init()
 
-def print_grid(grid):
-    #Affiche le plateau de jeu    
-    print("  _________________", str(score),"________________")
-    print("   A B C D E F G H I J K L M N O P Q R S")
-    print("  _____________________________________")
-    c = 'A'
-    for i in range(len(grid)):
-        print(c,"|", end="")
-        for j in range(len(grid[i])):
-            if grid[i][j] == 0:
-                print(" ", end=" ")
-            elif grid[i][j] == 1:
-                print(".", end=" ")
-            elif grid[i][j] == 2:
-                print("■", end=" ")
-        print()
-        c = chr(ord(c) + 1)
+# Définition de la taille de la fenêtre et création de la fenêtre
+largeur, hauteur = 800, 600
+fenetre = pygame.display.set_mode((largeur, hauteur))
 
-def select_bloc(reponse3):
-    #Affiche les blocs possibles
-    if reponse3 == 1:
-        print("Veuillez selectionner un bloc :")
-        for h in range(len(list_bloc)):
-            print("\n", h + 1, ": \n", end="")
-            for i in range(len(list_bloc[h])):
-                for j in range(len(list_bloc[h][i])):
-                    if list_bloc[h][i][j] == 1:
-                        print("■", end=" ")
-                    else:
-                        print(". ", end="")
-                print()
-    #Ajouter dans la liste bloc le bloc choisi par l'utilisateur 
-        n = int(input())
-        for i in range(len(list_bloc[n-1])):
-                bloc.append(list_bloc[n-1][i])
-    #Afficher le bloc choisi par l'utilisateur
-        print("Vous avez choisi :")
-        for i in range(len(list_bloc[n - 1])):
-            for j in range(len(list_bloc[n - 1][i])):
-                if list_bloc[n - 1][i][j] == 1:
-                    print("■", end=" ")
-                else:
-                    print(". ", end="")
-            print()
+# Titre de la fenêtre
+pygame.display.set_caption("Jeu avec personnage carré et plateformes")
 
-    if reponse3 == 2:
-        print("Veuillez selectionner un bloc :")
-        cpt = 0
-        alea = []
-        while cpt <3:
-            alea1 = random.randint(0,6)
-            alea.append(alea1)
-            print("\n", cpt+1, ": \n", end="")
-            for i in range(len(list_bloc[alea1])):
-                for j in range(len(list_bloc[alea1][i])):
-                    if list_bloc[alea1][i][j] == 1:
-                        print("■", end=" ")
-                    else:
-                        print(". ", end="")
-                print()
-            cpt +=1
-        n = int(input())
-        print("Vous avez choisi :" )
-        alea1=alea[n-1]
-        for i in range(len(list_bloc[alea1])):
-            bloc.append(list_bloc[alea1][i])
-    #Afficher le bloc
-        for i in range(len(list_bloc[alea1])):
-            for j in range(len(list_bloc[alea1][i])):
-                if list_bloc[alea1][i][j] == 1:
-                    print("■", end=" ")
-                else:
-                    print(". ", end="")
-            print()
-
-def valid_position(grid, bloc):
-    global Valid_pos, Xi, Yi
-    Valid_pos = True
-    x = input("Veuillez saisir la ligne souhaitée : ")
-    y = input("Veuillez saisir la colonne souhaitée : ")
-    X = Xi= ord(x)-97
-    Y= Yi =ord(y)-97
+# Classe Personnage
+class Personnage:
+    def __init__(self, x, y, taille):
+        self.x = x
+        self.y = y
+        self.taille = taille
+        self.vx = 0
+        self.vy = 0
     
-    X = X - len(bloc)+ 1
-    for i in range(len(bloc)):
-        if Valid_pos == False:
+    def appliquer_force(self, fx, fy):
+        self.vx += fx
+        self.vy += fy
+    
+    def maj_vitesse(self, gravite):
+        self.vy += gravite
+
+    
+    def deplacer(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def dessiner(self, fenetre):
+        pygame.draw.rect(fenetre, (255, 255, 255), (self.x, self.y, self.taille, self.taille))
+        
+    def collision(self, niveau, taille_bloc, largeur, hauteur):
+        x_min = self.x
+        y_min = self.y
+        x_max = self.x + self.taille
+        y_max = self.y + self.taille
+
+        # Collision avec les bords de la fenêtre
+        if x_min < 0:
+            self.x = 0
+            self.vx = 0
+        if y_min < 0:
+            self.y = 0
+            self.vy = 0
+        if x_max > largeur:
+            self.x = largeur - self.taille
+            self.vx = 0
+        if y_max > hauteur:
+            self.y = hauteur - self.taille
+            self.vy = 0
+
+        # Collision horizontale
+        i_min, j_min = int(y_min // taille_bloc), int(x_min // taille_bloc)
+        i_max, j_max = int(y_max // taille_bloc), int(x_max // taille_bloc)
+
+        for i in range(i_min, i_max + 1):
+            if i < 0 or i >= len(niveau):  # Vérification des limites pour i
+                continue
+            for j in range(j_min, j_max + 1):
+                if j < 0 or j >= len(niveau[0]):  # Vérification des limites pour j
+                    continue
+                if niveau[i][j] == 1:
+                    if self.vx > 0:
+                        self.x = j * taille_bloc - self.taille
+                        self.vx = 0
+                    elif self.vx < 0:
+                        self.x = (j + 1) * taille_bloc
+                        self.vx = 0
+                    break
+            else:
+                continue
             break
-        for j in range(len(bloc[i])):
-            if (bloc[i][j] + grid[X][Y] == 3) or (bloc[i][j] + grid[X][Y] == 0) or (bloc[i][j] + grid[X][Y] == 1):
-                Valid_pos = False
-                break
-            if bloc[i][j] + grid[X][Y] == 2:
-                Valid_pos = True
-            Y += 1  
-        X += 1
-        Y -= 3
 
-def emplace_bloc(grid, bloc, Xi, Yi, Valid_pos):
-    Xi = Xi - len(bloc)+ 1 
-    if Valid_pos == True:
-        for i in range(len(bloc)):
-            for j in range(len(bloc[i])):
-                if bloc[i][j] + grid[Xi][Yi] == 2:
-                   grid[Xi][Yi] = grid[Xi][Yi]+1
-                Yi += 1  
-            Xi += 1
-            Yi -= 3
-    else : 
-        print("VOUS NE POUVEZ PAS POSER LE BLOC ICI ! \n" )
+        # Collision verticale
+        x_min = self.x
+        y_min = self.y
+        x_max = self.x + self.taille
+        y_max = self.y + self.taille
 
-def row_state(grid):
-        global row_test, row_len
-        row_len = 0
-        for i in range(len(grid)):
-            if 1 in grid[i]:
-                row_test = False
-            else :
-                row_test = True
-                row_len = i
-                break
+        i_min, j_min = int(y_min // taille_bloc), int(x_min // taille_bloc)
+        i_max, j_max = int(y_max // taille_bloc), int(x_max // taille_bloc)
 
-def row_clear(grid, row_test, row_len, score):
-    if row_test == True:
-        for j in range(len(grid[row_len])):
-            if grid[row_len][j] == 2:
-                score = score + 1
+        for i in range(i_min, i_max + 1):
+            if i < 0 or i >= len(niveau):  # Vérification des limites pour i
+                continue
+            for j in range(j_min, j_max + 1):
+                if j < 0 or j >= len(niveau[0]):  # Vérification des limites pour j
+                    continue
+                if niveau[i][j] == 1:
+                    if self.vy > 0:
+                        self.y = i * taille_bloc - self.taille
+                        self.vy = 0
+                    elif self.vy < 0:
+                        self.y = (i + 1) * taille_bloc
+                        self.vy = 0
+                    break
+            else:
+                continue
+            break
+
+
+    def sur_une_plateforme(self, niveau, taille_bloc, tol=2):
+        x_min = self.x
+        y_min = self.y
+        x_max = self.x + self.taille
+        y_max = self.y + self.taille
+
+        i_min, j_min = int((y_min + tol) // taille_bloc), int((x_min + tol) // taille_bloc)
+        i_max, j_max = int((y_max - tol) // taille_bloc), int((x_max - tol) // taille_bloc)
+
+        for i in range(i_min, i_max + 1):
+            for j in range(j_min, j_max + 1):
+                if niveau[i][j] == 1:
+                    if self.vy >= 0 and y_max <= (i * taille_bloc) + tol:
+                        return True
+        return False
+
+# Création d'un personnage
+personnage = Personnage(largeur//2, hauteur//2, 40)
+
+# Vitesse du personnage
+vitesse = 1
+
+# Matrice représentant le niveau
+niveau = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+# Taille des blocs
+taille_bloc = 40
+
+def generer_points_parabole(x0, y0, vx, vy, gravite, nb_points):
+    points = []
+    for t in range(nb_points):
+        x = x0 + vx * t
+        y = y0 - (vy * t - 0.5 * gravite * t**2)
+        points.append((x, y))
+    return points
+
+def dessiner_parabole(fenetre, x0, y0, vx, vy, gravite, nb_points, couleur):
+    x, y = x0, y0
+    for t in range(1, nb_points):
+        x_prec, y_prec = x, y
+        x += vx
+        y += vy
+        vy += gravite
+        pygame.draw.line(fenetre, couleur, (x_prec, y_prec), (x, y), 2)
+
+
+
+
+# Fonction pour dessiner les plateformes
+def dessiner_plateformes(fenetre, niveau, taille_bloc):
+    for i, ligne in enumerate(niveau):
+        for j, bloc in enumerate(ligne):
+            if bloc:
+                pygame.draw.rect(fenetre, (0, 255, 0), (j * taille_bloc, i * taille_bloc, taille_bloc, taille_bloc))
+
+force_max = 200
+gravite = 1
+vitesse_lancer = 10
+deplacement_actif = False
+
+clic_initial = None
+
+
+while True:
+    gravite = 0.2
+    # Gestion des événements
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # Gestion des clics de la souris
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Bouton gauche de la souris
+                clic_initial = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1 and clic_initial:  # Bouton gauche de la souris relâché
+                cible_x, cible_y = pygame.mouse.get_pos()
+                dx = clic_initial[0] - cible_x
+                dy = clic_initial[1] - cible_y
+                distance = (dx ** 2 + dy ** 2) ** 0.5
+                rapport_force = min(1, distance / force_max)
+                if distance != 0:
+                    personnage.vx = dx * vitesse_lancer * rapport_force / distance
+                    personnage.vy = dy * vitesse_lancer * rapport_force / distance
+                else:
+                    personnage.vx = 0
+                    personnage.vy = 0
+
+                deplacement_actif = True
+                clic_initial = None
+
+    if not personnage.sur_une_plateforme(niveau, taille_bloc):
+        personnage.maj_vitesse(gravite)
+    else:
+        personnage.vy = 0
+
+
+    # Appliquer la gravité au personnage si le déplacement est actif
+    if deplacement_actif:
+        personnage.x += personnage.vx
+        personnage.y += personnage.vy
+        personnage.collision(niveau, taille_bloc, largeur, hauteur)
+
+
+
+    # Effacement de l'écran
+    fenetre.fill((0, 0, 0))
+
+    # Dessin des plateformes
+    dessiner_plateformes(fenetre, niveau, taille_bloc)
+
+    # Dessin du personnage
+    personnage.dessiner(fenetre)
+
+    # Dessin de la ligne de lancement si un clic initial est enregistré
+    if clic_initial:
+        cible_x, cible_y = pygame.mouse.get_pos()
+        dx = clic_initial[0] - cible_x
+        dy = clic_initial[1] - cible_y
+        distance = (dx ** 2 + dy ** 2) ** 0.5
+        rapport_force = min(1, distance / force_max)
+        if distance != 0:
+            vx = dx * vitesse_lancer * rapport_force / distance
+            vy = dy * vitesse_lancer * rapport_force / distance
+        else:
+            vx = 0
+            vy = 0
         
-        for j in range(len(grid[row_len])):
-                if grid[row_len][j] == 2 :
-                    grid[row_len][j] = 1          
-            
-        for k in range(row_len, 0, -1):
-            for j in range(len(grid[k])):
-                if grid[k-1][j] == 2:
-                    grid[k][j] = 2
-                    grid[k-1][j] = 1
+        nb_points_min = 10
+        nb_points_max = 100
+        nb_points = int(nb_points_min + rapport_force * (nb_points_max - nb_points_min))
+        
+        dessiner_parabole(fenetre, personnage.x + personnage.taille // 2, personnage.y + personnage.taille // 2, vx, vy, gravite, nb_points, (255, 0, 0))
 
-def update_score(row_test, score):
-    if row_test == True:
-        for j in range(len(grid[row_len])):
-            if grid[row_len][j] == 2:
-                score += 1
+    # Actualisation de l'écran
+    pygame.display.flip()
 
-    
-
-#------------------------------------#
-valeur()
-while reponse != 2 :
-    print("----------------------------------------------------------------------------------------\n")
-    print("--------------------------Bienvenue sur le jeu TETRIS ----------------------------------\n")
-    print("----------------------------------------------------------------------------------------\n")
-    print("Choisissez une action en entrant 1 ou 2 :\n")
-    print("1 - Afficher les règles du jeu \n")
-    print("2 - Commencer à jouer \n")
-    reponse = int(input())
-    while reponse < 0 or reponse > 2:
-        reponse = int(input(" Le chiffre n'est pas reconnu veuillez réessayer ") )
-    end = -1
-    if reponse == 1 :
-        while end == -1:
-            print("-----------------------------Règles du jeu --------------------------------------")
-            print("Le but du tétris est de placer des blocs sur un tableau de manière à la remplir entièrement")
-            print("Vous devez remplir le tableau en un minimum de coups possibles")
-            print("Vous ne pouvez pas placer un bloc sur une case déjà prise et il ne doit pas sortir du tableau\n")
-            print("Appuyer 1 pour revenir au menu principal :")
-            end = int(input())
-    if reponse == 2 :
-        print("-----------------------------Choix du plateau--------------------------------------")
-        print("Quelle forme de plateau voulez-vous ? Choisissez en entrant le chiffre correspondant : \n")
-        print("1 - Losange")
-        print("2 - Cercle")
-        print("3 - Triangle ")
-        path = int(input())
-        while path < 1 or path > 3:
-            path = int(input("Veuillez vérifier que votre chiffre est correct et réessayez "))
-        if path == 1:
-            print("Vous avez choisi le losange, Bonne chance !!\n")
-        elif path == 2:
-            print("Vous avez choisi le Cercle, Bonne chance !!\n")
-        elif path == 3:
-            print("Vous avez choisi le Triangle, Bonne chance !! \n")
-        print("--------------------------Mode de Jeu ----------------------------------")
-    print("Choisissez un mode de jeu en entrant 1 ou 2 :\n")
-    print("1 - Choix manuel des blocs ")
-    print("2 - 3 blocs sélectionnés aléatoirement \n")
-    reponse3 = int(input())
-    while reponse3 < 0 or reponse3 > 2:
-        reponse3 = int(input(" Le chiffre n'est pas reconnu veuillez réessayer ") )
-read_grid(path, grid)
-print_grid(grid)
-select_bloc(reponse3)
-while Testjeu == False:
-    valid_position(grid, bloc)
-    emplace_bloc(grid, bloc, Xi, Yi, Valid_pos)
-    row_state(grid)
-    update_score(row_test, score)
-    row_clear(grid, row_test, row_len, score)
-    bloc = []
-    print_grid(grid)
-    select_bloc(reponse3)
-    
-    
+    # Limiter les images par seconde
+    pygame.time.Clock().tick(60)
